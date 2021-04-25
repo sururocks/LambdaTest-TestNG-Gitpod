a2dd8a2e82d032970c3903e29a114acd38c3abb9
sururocks <70531685+sururocks@users.noreply.github.com>
2021-04-25 18:23:51 +0530

    Create startTunnel.sh

---
+++ startTunnel.sh
@@ -0,0 +1,5 @@
+wget "https://downloads.lambdatest.com/tunnel/v3/linux/64bit/LT_Linux.zip" -P LT_Linux/
+unzip -o LT_Linux/LT_Linux.zip -d LT_Linux
+rm -f -- LT_Linux/LT_Linux.zip
+cd LT_Linux
+./LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
