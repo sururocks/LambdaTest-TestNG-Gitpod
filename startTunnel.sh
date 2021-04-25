2c8f84442c2f50def10346599829fb8b03884094
sururocks <70531685+sururocks@users.noreply.github.com>
2021-04-25 19:18:46 +0530

    Update startTunnel.sh

--- startTunnel.sh
+++ startTunnel.sh
@@ -5,6 +5,11 @@ chmod +x /usr/local/bin/LT
 mkdir LT_Linux
 wget "https://downloads.lambdatest.com/tunnel/v3/linux/64bit/LT_Linux.zip" -P LT_Linux/
 unzip -o LT_Linux/LT_Linux.zip -d LT_Linux
+
+cp LT /usr/local/bin/
+chmod +x /usr/local/bin/LT
+./usr/local/bin/LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
+
 sleep 5
 cd LT_Linux
 ./LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
