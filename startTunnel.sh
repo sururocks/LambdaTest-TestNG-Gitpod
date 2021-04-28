1b73b289e1626a1f2cc25d758f8446474d0f3ab2
sururocks <70531685+sururocks@users.noreply.github.com>
2021-04-28 17:48:05 +0530

    Update startTunnel.sh

--- startTunnel.sh
+++ startTunnel.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-cp LT /usr/local/bin/
-chmod +x /usr/local/bin/LT
-./usr/local/bin/LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
 mkdir LT_Linux
 wget "https://downloads.lambdatest.com/tunnel/v3/linux/64bit/LT_Linux.zip" -P LT_Linux/
 unzip -o LT_Linux/LT_Linux.zip -d LT_Linux
 
-cp LT /usr/local/bin/
-chmod +x /usr/local/bin/LT
-./usr/local/bin/LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
+chmod 755 LT_Linux/LT
+
+./LT_Linux/LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
 
 sleep 5
-cd LT_Linux
+unzip -o LT_Linux.zip
+chmod 755 LT
 ./LT --user surendran --key PdbbYUORRqDdBo6isuHzxOGKPmburjYhHww7qS0wr0qdZoNFDZ --tunnelName GCloud --env prod --verbose  --mitm --load-balanced &
